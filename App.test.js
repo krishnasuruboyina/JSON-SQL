a0b6f484af4ebd3dba123877e210be9f831ab39f
krishnasuruboyina <krishnasuriboyina@gmail.com>
2026-07-13 15:53:33 +0530

    Update App.test.js

--- App.test.js
+++ App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the JSONSQL heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/JSONSQL - JSON to POSTGRESQL/i);
+  expect(heading).toBeInTheDocument();
 });
